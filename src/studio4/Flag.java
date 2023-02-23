c51b73fff89022c040399c11a2eee639b7c28c07
drewkassman <drewkassman@localhost>
2023-02-23 14:14:40 -0600

    jshdfsydgf

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,17 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.rectangle(.5, .5, .45, .3);
+		StdDraw.setPenColor(91, 113, 210);
+		StdDraw.filledRectangle(.35, .45, .2, .15); //whale body
+		StdDraw.filledRectangle(.63, .35 , .1, .05); // whale tail
+		StdDraw.filledRectangle(.7777777, .4 , .05, .1); //
+		
+		double [] x = {.63, .63, .7777777};
+		double [] y = {.8, .5, .5}; 
+		StdDraw.filledPolygon (x, y);
+		StdDraw.setPenColor(181, 122, 200);
+		}
+		
 	}
-}
+
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,5 @@
 package studio4;
-
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +20,21 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		String shapeType = in.next();
+		System.out.println(shapeType);
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		StdDraw.setPenColor(red, green, blue);
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		
 	}
+	
 }
